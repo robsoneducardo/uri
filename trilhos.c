7a7b6954b7fbfc45c40fd494fc28f1d4b101860d
robsoneducardo <robsoneducardo@gmail.com>
2020-03-17 16:43:05 -0300

    Trilhos.c adicionado, mas não funciona

--- trilhos.c
+++ trilhos.c
@@ -4,35 +4,58 @@ void main(int argc, char** argv){
     char entrada[27], saida[27], pilha[27]={0}, x;
     int entr=0, said=0, topo=0;
     scanf("%d", &nVag);
+    getchar();
     while (nVag>0){
         int i;
+        entr=0;
+        said=0;
+        topo=0;
         for (i=0; i<nVag; ++i){
-            scanf ("%c", entrada+i);
             scanf ("%c", &x);
+            if (i!=nVag-1) getchar();
+            entrada[i]=x;
         }
+        getchar();
         for (i=0; i<nVag; ++i){
-            scanf ("%c", &saida[i]);
             scanf ("%c", &x);
+            if (i!=nVag-1) getchar();
+            saida[i]=x;
         }
-        printf("%s\n", entrada);
-        printf("%s\n", saida);
+        entrada[nVag]=0;
+        saida[nVag]=0;
+        //dados do problema lidos.
         while (said<nVag){
+            printf("%s\n%s\n%s\n\n", entrada, pilha, saida);
+            //se o topo da pilha concorda com a saída:
             if (topo>0 && pilha[topo-1] == saida[said]){
+                //tira da pilha para a saida
+                //printf("topo:%d\n", topo);
                 topo--;
                 said++;
-                printf("I");
+                printf("R");
+                //se zerou tudo:
+                if (entr==nVag && topo==0){
+                    //resolveu o problema.
+                    printf("\n");
+                    break;
+                }
             }
+            // se a pilha não concorda e tem entrada:
             else if (entr<nVag){
+                //tras da entrada pra ela.
+                //printf("topo->%d\n", topo);
                 pilha[topo]=entrada[entr];
                 topo++;
                 entr++;
-                printf("R");
+                printf("I");
             }
+            // se a pilha não concorda e não tem entrada (deu ruim)
             else{
                 printf (" Impossible\n");
                 break;
             }
         }
+        printf(">");
         scanf("%d", &nVag);
     }
     return;
